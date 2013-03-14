c6a57b98828283fffd194adac9ebc83fdc81487b
Pc <Pc@Pc-PC>
2013-03-14 19:14:57 +0000

    Mais javardice

--- src/testedespesas/TesteDespesas.java
+++ src/testedespesas/TesteDespesas.java
@@ -1,21 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package testedespesas;
 
-/**
- *
- * @author Pc
- */
 public class TesteDespesas {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-       
-        
-        
     }
 }
