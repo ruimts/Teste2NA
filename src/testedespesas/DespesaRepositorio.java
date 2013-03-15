d60f846a27aae021f32112550a7f04e84abbe056
Pc <Pc@Pc-PC>
2013-03-15 23:01:27 +0000

    Registo de Despesa3

--- src/testedespesas/DespesaRepositorio.java
+++ src/testedespesas/DespesaRepositorio.java
@@ -1,13 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package testedespesas;
 
-/**
- *
- * @author Pc
- */
 public class DespesaRepositorio {
-    
+
+//    public void Grava(Despesa) {
+//    }
 }
