3206545db1f6f502b9bb3d494eb951ca3de9a1f5
Pc <Pc@Pc-PC>
2013-03-15 23:08:27 +0000

    Registo de Despesa4

--- src/testedespesas/Despesa.java
+++ src/testedespesas/Despesa.java
@@ -1,13 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package testedespesas;
 
-/**
- *
- * @author Pc
- */
 public class Despesa {
-    
+
+    private int valor;
+    private String desc;
+
+    public void Despesa(int v, String d) {
+        this.valor = v;
+        this.desc = d;
+    }
 }
